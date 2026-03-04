33a5fce862bf15a80512389f351c4a0f23e3e18e
Ashking <Ashking21@proton.me>
2026-03-05 00:29:44 +0530

    adding half assed loadHostKey func

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"encoding/pem"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,14 +20,51 @@ func loadHostKey(keyFile string) (ssh.Signer,error){
 			return nil,fmt.Errorf("failed to read key: %w",err)
 			
 		}
-		//key exists
+		//generate key
+	}
+	
+	 
 		signer,err := ssh.ParsePrivateKey(keyBytes)
-		if err == nil {
-			return signer,nil
+		if err != nil {
+			return nil,fmt.Errorf("failed to parse key : %w",err)
 		}
-	}	
+
+		return signer,nil
 }
 
+
+func generateHostKey(keyFile string) ([]byte,error){
+	_,privateKey,err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil,fmt.Errorf("something went wrong %w",err)
+	}
+	
+	
+	derBytes,err:= x509.MarshalPKCS8PrivateKey(privateKey) 
+
+	if err != nil {
+		return nil,fmt.Errorf("something went wrong %w",err)
+	}
+
+
+	block := &pem.Block{
+		Type: "OPENSSH PRIVATE KEY",
+		 Bytes: derBytes, 
+	}
+
+	pemBytes := pem.EncodeToMemory(block) 
+	 
+
+	if err := pem.Encode(os.Stdout, block); err != nil {
+		log.Fatal(err)
+	}
+	
+	if err != nil {
+		return nil,fmt.Errorf("something went wrong %w",err)
+	}
+
+	return nil,pemBytes
+}
 
 func main(){
 	//load or generate host key
